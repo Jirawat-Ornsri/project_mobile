44de97508ef41e772ef73e13cbac3c6448520016
Jirawat-Ornsri <jirawat.ornsri@gmail.com>
2024-04-11 21:44:53 +0700

    disable ip

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -7,7 +7,7 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GMSServices.provideAPIKey("AIzaSyDzpznrFgybdPnQQKq-F2xIlnsOcjCwV_Q")
+    // GMSServices.provideAPIKey("AIzaSyDzpznrFgybdPnQQKq-F2xIlnsOcjCwV_Q")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
